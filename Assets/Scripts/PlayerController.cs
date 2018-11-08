683f0e578e8647f6f3317b27e8b27f3b80a119e2
bdicicco <31937421+bcdicicco@users.noreply.github.com>
2018-11-08 02:08:45 -0500

    Upload
    Upload base program

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int redKeys;
+    public int blueKeys;
+    public int yellowKeys;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Red"))
+        {
+            other.gameObject.SetActive(false);
+            redKeys += 1;
+            // update red key UI
+        }
+
+        else if (other.gameObject.CompareTag("Blue"))
+        {
+            other.gameObject.SetActive(false);
+            blueKeys += 1;
+            // update blue key UI
+        }
+
+        else if (other.gameObject.CompareTag("Yellow"))
+        {
+            other.gameObject.SetActive(false);
+            yellowKeys += 1;
+            // update yellow key UI
+        }
+    }
+}
