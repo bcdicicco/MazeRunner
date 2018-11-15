d9655932c91f14ea742ef837fcbd536df7db36d0
bdicicco <31937421+bcdicicco@users.noreply.github.com>
2018-11-15 01:41:30 -0500

    Added title screen
    Title screen with play option that leads to the game. Leaderboard goes to the leaderboard that is currently not done. Exit will close the application.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    public 
+
 
 
 
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,12 +7,8 @@ public class Timer : MonoBehaviour
     public Text timerText;
     private float startTime;
     private string finalTime;
+    private bool start = false;
     private bool finished = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        startTime = Time.time;
-    }
 
     // Update is called once per frame
     void Update()
@@ -22,16 +18,21 @@ public class Timer : MonoBehaviour
             return;
         }
 
-        float timeDiff = Time.time - startTime;
+        if (start)
+        { 
 
-        string minutes = ((int)timeDiff / 60).ToString();
-        string seconds = (timeDiff % 60).ToString("f0");
-        if (seconds == "0" || seconds == "1" || seconds == "2" || seconds == "3" || seconds == "4" || seconds == "5" || seconds == "6" || seconds == "7" || seconds == "8" || seconds == "9")
-        {
-            seconds = "0" + seconds;
+            float timeDiff = Time.time - startTime;
+
+            string minutes = ((int)timeDiff / 60).ToString();
+            string seconds = (timeDiff % 60).ToString("f0");
+
+            if (seconds == "0" || seconds == "1" || seconds == "2" || seconds == "3" || seconds == "4" || seconds == "5" || seconds == "6" || seconds == "7" || seconds == "8" || seconds == "9")
+            {
+                seconds = "0" + seconds;
+            }
+
+            timerText.text = minutes + ":" + seconds;
         }
-
-        timerText.text = minutes + ":" + seconds;
     }
 
     public void StopTimer()
@@ -40,9 +41,19 @@ public class Timer : MonoBehaviour
         timerText.color = Color.yellow;
     }
 
+    public void StartTimer()
+    {
+        startTime = Time.time;
+        start = true;
+    }
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Start"))
+            StartTimer();
+    
+
         if (other.gameObject.CompareTag("Finish"))
             StopTimer();
+
     }
 }
